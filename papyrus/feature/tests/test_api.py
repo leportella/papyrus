bd6c22aea75e63a518badf8b3fd60d14bc4da6b0
Leticia Portella <leportella@gmail.com>
2018-08-10 14:15:16 -0300

    add more tests to test feature api

--- papyrus/feature/tests/test_api.py
+++ papyrus/feature/tests/test_api.py
@@ -5,49 +5,64 @@ import pytest
 from .factories import FeatureFactory, UserFactory
 
 
-@pytest.fixture
-def data():
+def data(user):
     return {
         'description': 'This is a description',
         'priority': 1,
         'product_area': '4',
         'target_date': '2018-08-08',
-        'title': 'Title',
+        'title': 'new_priority_1',
+        'client': user.id,
     }
 
+def post(client, url, body):
+    response = client.post(url, body, content_type="application/json")
+    return json.loads(response.content)
+
+@pytest.fixture
+def user():
+    user = UserFactory()
+    FeatureFactory(title='priority_1', client=user, priority=1)
+    FeatureFactory(title='priority_2', client=user, priority=2)
+    return user
+
 
 @pytest.mark.django_db
-def test_list_features(client):
-    FeatureFactory()
+def test_list_features(client, user):
     response = client.get('/features/')
     content = json.loads(response.content)
-    assert len(content['objects']) == 1
+    assert len(content['objects']) == 2
 
 
 @pytest.mark.django_db
-def test_create_feature(client, data):
-    user = UserFactory()
-    data.update({'client': user.id})
-    response = client.post('/features/', json.dumps(data),
-                           content_type="application/json")
-    content = json.loads(response.content)
+def test_create_feature_reprioritizing(client, user):
+    body = json.dumps(data(user))
+    content = post(client, '/features/', body)
+
     assert content['priority'] == 1
-    assert content['feature_title'] == 'Title'
+    assert content['feature_title'] == 'new_priority_1'
     assert content['client_name'] == user.email
+
+    for feature in user.features.all():
+        if feature.title == 'new_priority_1':
+            assert feature.priority == 1
+        if feature.title == 'priority_1':
+            assert feature.priority == 2
+        if feature.title == 'priority_2':
+            assert feature.priority == 3
 
 
 @pytest.mark.django_db
-def test_create_feature_reprioritizing(client, data):
-    user = UserFactory()
-    data.update({'client': user.id})
-    feature = FeatureFactory(priority=1, client=user)
-
-    response = client.post('/features/', json.dumps(data),
-                           content_type="application/json")
-    content = json.loads(response.content)
-    assert content['priority'] == 1
-    assert content['feature_title'] == 'Title'
-    assert content['client_name'] == user.email
-
-    feature.refresh_from_db()
-    assert feature.priority == 2
+@pytest.mark.parametrize('attribute', [
+    'description',
+    'priority',
+    'product_area',
+    'target_date',
+    'client',
+    'title'
+])
+def test_create_feature_errors(client, user, attribute):
+    body = data(user)
+    body.pop(attribute)
+    content = post(client, '/features/', body)
+    assert content['error'][attribute] ==  ['This field is required.']
