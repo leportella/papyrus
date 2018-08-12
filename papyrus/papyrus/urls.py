f941069a152c785bb0ce2cd2669f05c2830d4b15
Leticia Portella <leportella@gmail.com>
2018-08-12 20:58:48 -0300

    add "api" to endpoints that are apis

--- papyrus/papyrus/urls.py
+++ papyrus/papyrus/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from feature.api import FeatureResource
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('features/', include(FeatureResource.urls())),
+    path('api/features/', include(FeatureResource.urls())),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
