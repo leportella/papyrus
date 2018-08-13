592825bb512c49f5b74939bfdb755172e5c34e73
Leticia Portella <leportella@gmail.com>
2018-08-13 19:46:25 -0300

    deploy libs installed

--- papyrus/papyrus/settings.py
+++ papyrus/papyrus/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
 from decouple import Csv, config
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
